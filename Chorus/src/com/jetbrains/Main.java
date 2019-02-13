8cafbb549bc24ae7bd7c73f44eac719efa2c358b
clairantoine <46975462+clairantoine@users.noreply.github.com>
2019-02-13 00:32:49 -0600

    Add files via upload

---
+++ Chorus/src/com/jetbrains/Main.java
@@ -0,0 +1,15 @@
+package com.jetbrains;
+
+public class Chorus {
+
+    public static void oldMan()
+    {System.out.println("With a knick-knack paddywhack,\nGive the dog a bone\nThis old man came rolling home.");
+    }
+
+    {
+        public static void main(String[] args); {
+            System.out.print("This old man, he played 1\nHe played knick-knack on my thumb");
+            oldMan();
+
+    }
+}
