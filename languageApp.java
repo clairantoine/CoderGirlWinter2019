19bc73a4b8a68d92d7b45dbe4b8f50420ce03061
clairantoine <46975462+clairantoine@users.noreply.github.com>
2019-02-15 20:10:35 -0600

    Add files via upload

---
+++ languageApp.java
@@ -0,0 +1,34 @@
+package com.jetbrains;
+
+import java.util.Scanner;
+
+public class languageApp(){
+
+        public static void languageApp()
+        {Scanner keyboard = new Scanner(System.in);
+            System.out.println("What's your name?");
+        String userName = keyboard.nextLine();
+        System.out.println("What is your language preference?"+
+            "Please select 1 for English; 2 for French; or 3 for Spanish.");
+        String languageEnglish= (1);
+        String languageFrench= (2);
+        String languageSpanish= (3);
+        language = keyboard.nextLine()
+        if (language == 1)
+            System.out.println("Hello, "+username+"!");
+        if (language == 2)
+            System.out.println("Bonjour, "+username+"!");
+        if (language == 3)
+            System.out.println("Hola "+username+"!");
+        else ("Error: Please select 1, 2, or 3")
+        { }
+        }
+
+
+    public static void main(String[] args) {
+    languageApp();
+
+
+
+    }
+}
