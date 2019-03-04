abafe260d4074e0b10e25f6f3a19239c3550cc61
clairantoine <46975462+clairantoine@users.noreply.github.com>
2019-03-04 16:25:37 -0600

    Small exercise on While Loops

---
+++ countTo30/src/com/jetbrains/Main.java
@@ -0,0 +1,16 @@
+package com.jetbrains;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        int number= 0;
+
+            while (number < 30)
+            {
+                number = number + 1;
+                System.out.println(number);
+            }
+
+    }
+}
