f1a8d24c5e636f11a3fb22e676fd6ca6c4a4a60e
clairantoine <46975462+clairantoine@users.noreply.github.com>
2019-04-10 23:28:57 -0500

    Temperatures Exercise

--- Main.java
+++ Main.java
@@ -1,31 +1,73 @@
 package com.jetbrains;
-import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner keyboard = new Scanner(System.in);
-        System.out.print ("Welcome to the Multiplication Tables");
-        System.out.print("\n" +
-                "How large would you like to see them?\n" +
-                "(enter a number smaller than 20)");
+    public static void main(String[] args)
+    {
+     int temps [][]= new int [4][7];
+                temps[0][0]= 68;
+                temps[0][1]= 70;
+                temps[0][2]= 76;
+                temps[0][3]= 70;
+                temps[0][4]= 68;
+                temps[0][5]= 71;
+                temps[0][6]= 75;
+                temps[1][0]= 76;
+                temps[1][1]= 76;
+                temps[1][2]= 87;
+                temps[1][3]= 84;
+                temps[1][4]= 82;
+                temps[1][5]= 75;
+                temps[1][6]= 83;
+                temps[2][0]= 73;
+                temps[2][1]= 72;
+                temps[2][2]= 81;
+                temps[2][3]= 78;
+                temps[2][4]= 76;
+                temps[2][5]= 73;
+                temps[2][6]= 77;
+                temps[3][0]= 64;
+                temps[3][1]= 65;
+                temps[3][2]= 69;
+                temps[3][3]= 68;
+                temps[3][4]= 70;
+                temps[3][5]= 74;
+                temps[3][6]= 72;
 
-        int userNumber = keyboard.nextInt();
-        keyboard.skip("\n");
-        System.out.print(" "+userNumber);
-        int counter = 0;
+        String timeOfDay[]= {"7AM", "3PM", "7PM", "3AM"};
 
-        if (userNumber >= 20)
+        System.out.print("Temperature Calculator");
+        System.out.println("The data provided are:");
+     for(int row=0; row<4; row++)
         {
-            System.out.println("Error. Please enter a number smaller than 20");
+            System.out.print(timeOfDay[row]+": ");
+                for(int column= 0; column<7; column++)
+         {
+                if (column<6)
+                {
+                    System.out.print(temps[row][column] + ", ");
+                }
+                else
+                 {
+                    System.out.print(temps[row][column]+" ");
+                 }
+         }
+         System.out.println();
         }
-        else
+        System.out.println("\nBased on that data, the following are the average temperatures for the week\n");
+
+
+     String daysOfWeek[]= {"Sun:", "Mon:", "Tue:", "Wed:", "Thu:", "Fri:", "Sat:"};
+//Loop for average temp of each time slot
+        int daysLoop = 0;
+        int workingAmount = 0;
+        for( int times = 0 ; times<4; times++)
         {
-            for (int answer= 0; counter < userNumber; counter++)
-            { answer= counter * userNumber;
-                System.out.println("" +counter+ " * " +userNumber+ " = " +answer+"");
-            }
+         workingAmount = workingAmount + temps[times][daysLoop];
+            System.out.println(workingAmount/4);
         }
+
+
 
     }
 }
